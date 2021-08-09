b9e6eea63815cd42132a7eafd56bd5cecc0fd93f
Abdul Manaf P <abdulmanaf469421@gmail.com>
2021-08-09 17:21:49 +0530

    validation mistake

--- StudentResultPublishing/Models/LoginModel.cs
+++ StudentResultPublishing/Models/LoginModel.cs
@@ -5,10 +5,9 @@ namespace StudentResultPublishing
    public class LoginModel
     {
         [Required(ErrorMessage ="Enter valid Email")]
-        [Display(Name = "Email")]
+        [EmailAddress]
         public string UserName { get; set; }
         [Required(ErrorMessage ="Invalid Password")]
-        [Display(Name ="Password")]
         public string Password { get; set; }
     }
 }
